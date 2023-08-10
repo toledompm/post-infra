61f10bc30f0c8cf3e65cf3ff70388be4644f401a
toledompm <toledompm@gmail.com>
2023-08-10 12:18:26 -0300

    fix: next config

--- docker/blog/next.config.js
+++ docker/blog/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["source.unsplash.com", "blog.toledompm.xyz"],
+    domains: ["source.unsplash.com", "objectstorage.sa-saopaulo-1.oraclecloud.com"],
   },
 };
 
